b0ada7d3f9ad97b9dd1d17dd25207ec971c359ba
keerthypriya2105 <keerthypriya2105@gmail.com>
2025-10-27 00:18:09 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,9 +17,11 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
+    historyApiFallback: true,
   },
   preview: {
     port: 4173,
     host: true,
+    historyApiFallback: true,
   },
 })
